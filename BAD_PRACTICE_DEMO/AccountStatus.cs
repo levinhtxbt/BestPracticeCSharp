501a3d12f9bb2c1a66d01759ec5865e994dd55f0
Le Vinh <levinhtxbt@gmail.com>
2016-12-23 16:27:38 +0700

    refactor code without Dependency Injection

---
+++ BAD_PRACTICE_DEMO/AccountStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAD_PRACTICE_DEMO
+{
+    public enum AccountStatus
+    {
+        NotRegistered = 1,
+        SimpleCustomer = 2,
+        ValuableCustomer = 3,
+        MostValuableCustomer = 4
+    }
+}
---
+++ BAD_PRACTICE_DEMO/DiscountManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAD_PRACTICE_DEMO
+{
+    public class DiscountManager
+    {
+        public decimal ApplyDiscount(decimal price, AccountStatus accountStatus, int timeOfHavingAccountInYears)
+        {
+            decimal priceAfterDiscount = 0;
+            switch (accountStatus)
+            {
+                case AccountStatus.NotRegistered:
+                    priceAfterDiscount = price;
+                    break;
+                case AccountStatus.SimpleCustomer:
+                    priceAfterDiscount = price.ApplyDiscountForAccountStatus(Constants.DISCOUNT_FOR_SIMPLE_CUSTOMERS);
+                    break;
+                case AccountStatus.ValuableCustomer:
+                    priceAfterDiscount = price.ApplyDiscountForAccountStatus(Constants.DISCOUNT_FOR_VALUABLE_CUSTOMERS);
+                    break;
+                case AccountStatus.MostValuableCustomer:
+                    priceAfterDiscount = price.ApplyDiscountForAccountStatus(Constants.DISCOUNT_FOR_MOST_VALUABLE_CUSTOMERS);
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+            return priceAfterDiscount.ApplyDiscountForTimeOfHavingAccount(timeOfHavingAccountInYears);
+        }
+
+    }
+
+    
+}
